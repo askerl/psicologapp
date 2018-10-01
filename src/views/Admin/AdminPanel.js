6ec86799a605eae04ca9777240907450c35f4a41
Alfredo Skerl <Alfredo.X.Skerl.-nd@disney.com>
2018-10-01 11:52:07 -0300

    - Cleanup previo a liberación v3.2.0 - Oculto restaurar respaldo hasta solucionar tema de los 500 commits por batch

--- src/views/Admin/AdminPanel.js
+++ src/views/Admin/AdminPanel.js
@@ -9,46 +9,10 @@ class AdminPanel extends Component {
   
 	constructor(props) {
 		super(props);
-		this.actualizarValor = this.actualizarValor.bind(this);
 	}
 
 	componentDidMount() {
 		// nada por ahora
-
-	}
-
-	actualizarValor() {
-		// funcion auxiliar para actualizar modelo de datos de pagosPrepaga
-		// a valorConsulta
-
-		db.collection("pacientes").where("tipo","==",pacientePrepaga).get().then( querySnapshot => {
-			console.log('Pacientes Prepaga:', querySnapshot.docs.length);
-			// Get a new write batch
-			let batch = db.batch();
-
-			querySnapshot.docs.forEach( doc => {
-				let pacRef = doc.ref;
-				let paciente = doc.data();
-				console.log('Paciente', paciente.nombre, paciente.apellido);
-				console.log('Prepaga', paciente.prepaga);
-				console.log('Pago', paciente.pago);
-				console.log('Valor', prepagasById[paciente.prepaga].pagos[paciente.pago]);
-				let newValorConsulta = prepagasById[paciente.prepaga].pagos[paciente.pago];
-				// actualizo campo valorConsulta 
-				batch.update(pacRef,{valorConsulta: newValorConsulta});
-			});
-			
-			//Commit the batch
-			batch.commit().then( () => {
-				console.log('Datos actualizados');
-			}).catch( error => {
-				console.log('Error', error);
-			});
-
-		});
-
-
-
 	}
 
 	render() {
@@ -57,9 +21,6 @@ class AdminPanel extends Component {
 				<Row>
 					<Col>
 						<Respaldos/>
-						<Button color="success" size="sm" title="Actualizar Valor Consulta" onClick={this.actualizarValor}>
-							Actualizar Valor Consulta
-						</Button>                                    
 					</Col>
 				</Row>
 			</div>
